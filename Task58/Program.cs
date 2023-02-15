7018aa6dc660d68d037ffc44e36941a77d81ea33
Kirill <munnin95@gmail.com>
2023-02-15 12:34:30 +0300

    Complete 58 task

---
+++ Task58/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+Console.WriteLine("Введите количество строк:");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов:");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[rows, columns];
+int[,] secondMatrix = new int[rows, columns];
+int[,] resultMatrix = new int[rows, columns];
+
+void CreateMatrixRndInt(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintMaitrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+CreateMatrixRndInt(matrix);
+PrintMaitrix(matrix);
+Console.WriteLine();
+CreateMatrixRndInt(secondMatrix);
+PrintMaitrix(secondMatrix);
+
+if (matrix.GetLength(0) != secondMatrix.GetLength(1))
+{
+    Console.WriteLine(" Нельзя перемножить ");
+    return;
+}
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < secondMatrix.GetLength(1); j++)
+    {
+        resultMatrix[i, j] = 0;
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            resultMatrix[i, j] += matrix[i, k] * secondMatrix[k, j];
+        }
+    }
+}
+
+Console.WriteLine();
+PrintMaitrix(resultMatrix);
